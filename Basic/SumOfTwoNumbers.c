a87f7846c8607e1eaf1659597245061d8f3cb38f
Harshal <harshaltapre27@gmail.com>
2025-07-20 00:03:12 +0530

    Sum of two numbers basic program in C language

---
+++ Basic/SumOfTwoNumbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int nu1, num2, sum;
+    printf("Enter the Number 1: ");
+    scanf("%d", &nu1);
+    printf("Enter the Number 2: ");
+    scanf("%d", &num2);
+    sum = nu1 + num2;
+    printf("The Sum is: %d\n", sum);
+    return 0;
+}
