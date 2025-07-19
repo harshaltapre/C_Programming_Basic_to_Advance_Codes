ff91b13a64b80a63fef651fbc038cf3677714b28
Harshal <harshaltapre27@gmail.com>
2025-07-19 23:54:24 +0530

    new file:   .vscode/launch.json 	new file:   Sample.c 	new file:   a.exe

---
+++ Sample.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int Age , Height ;
+	printf( " Details of Person :\n");
+	printf( " Enter the Age :" );
+	scanf("%d",&Age);
+	printf("Enter the Height :");
+	scanf("%d",&Height);
+	printf("Details of Person :\n");
+	printf("Age is : %d\n",Age);
+	printf("Height is : %d\n",Height);
+    return 0;
+}
