b8df351970f744ae98be799175c3d5ba6810cc55
Harshal <harshaltapre27@gmail.com>
2025-07-20 01:42:51 +0530

    Refactor Area of Circle program to include alternative implementation using pow() and add VSCode settings file

--- Basic/AreaOfCircele.c
+++ Basic/AreaOfCircele.c
@@ -10,3 +10,21 @@ int main(){
 	printf("Area of Circle :%d",area);
 return 0;
 }
+
+
+/*
+#include <stdio.h>
+#define PI 3.14
+#include <math.h> //math.h header file for the POW so we can do a^v 
+int main(){
+int radius;  // for radius of circle in integer if you want float then change int to float
+	printf("Area of Circle calculator\n");
+	printf("Enter the Radius of Circle :\t");
+	scanf("%d",&radius);
+	int area;
+	area =PI*pow(radius,2); // area = π * r^2
+	printf("Area of Circle :%d",area);
+return 0;
+}
+
+*/
