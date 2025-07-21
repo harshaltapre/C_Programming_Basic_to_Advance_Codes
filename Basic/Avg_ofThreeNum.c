ea430f76330bd13a8fd4a0ebe59636a042ac2316
Harshal <harshaltapre27@gmail.com>
2025-07-22 01:01:24 +0530

    Add Average of Three Numbers program in C

---
+++ Basic/Avg_ofThreeNum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+	//Avrage of three numbers
+	int num1,num2,num3;
+	printf("Average of three Numbers Program :\n");
+	printf("Enter the Num 1:\n");
+	scanf("%d",&num1);
+	printf("Enter the Num 2:\n");
+	scanf("%d",&num2);
+	printf("Enter the Num 3:\n");
+	scanf("%d",&num3);
+	printf("Average of Three Numbers is : %d \n",(num1+num2+num3)/3);
+
+return 0;
+}
