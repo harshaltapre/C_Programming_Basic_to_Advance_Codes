5c99b56a9ad5c4dde8fd98a000081b7cd0bd2659
Harshal <harshaltapre27@gmail.com>
2025-07-22 01:20:39 +0530

    Add Ternary Operator demonstration program in C

---
+++ Basic/TernaryOperator.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+	// return smallest number 
+	printf(" Use of Ternary Operator \n");
+	int Age;
+	printf("Enter the Age:");
+	scanf("%d",&Age);
+	Age >=18 ? printf("Adult"):printf("Minor");
+return 0;
+}
